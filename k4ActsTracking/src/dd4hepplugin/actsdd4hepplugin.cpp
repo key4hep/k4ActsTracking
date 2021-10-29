fbc1a2a0819f10e0782b7edccf4d9719a20dd4fc
Andre Sailer <andre.philippe.sailer@cern.ch>
2021-10-29 13:56:55 +0200

    k4ActsPlugin: add some show casing of using cellIDcoder to identify layers

--- k4ActsTracking/src/dd4hepplugin/actsdd4hepplugin.cpp
+++ k4ActsTracking/src/dd4hepplugin/actsdd4hepplugin.cpp
@@ -3,8 +3,11 @@
 #include <DD4hep/Factories.h>
 #include <DD4hep/Printout.h>
 
+#include <DD4hep/IDDescriptor.h>
+
 #include <DDRec/DetectorData.h>
 #include <DDRec/SurfaceHelper.h>
+#include <DDRec/SurfaceManager.h>
 
 #include <string>
 
@@ -17,22 +20,47 @@ namespace {
    *
    */
 
-  static long addActsExtensions(dd4hep::Detector& description, int argc, char** argv) {
+  static long addActsExtensions(dd4hep::Detector& description, int, char**) {
     const std::string LOG_SOURCE("AddActsExtensions");
-    printout(PrintLevel::INFO, LOG_SOURCE, "Running extension");
+    printout(PrintLevel::INFO, LOG_SOURCE, "Running plugin");
+
+    //Getting the surface manager
+    dd4hep::rec::SurfaceManager& surfMan = *description.extension<dd4hep::rec::SurfaceManager>();
 
     dd4hep::rec::SurfaceHelper      ds(description.world());
-    dd4hep::rec::SurfaceList const& detSL = ds.surfaceList();
-
+    dd4hep::rec::SurfaceList const& detSL   = ds.surfaceList();
+    int                             counter = 0;
     for (dd4hep::rec::ISurface* surf : detSL) {
       dd4hep::Volume volume     = ((dd4hep::rec::Surface*)surf)->volume();
       auto*          ddsurf     = ((dd4hep::rec::Surface*)surf);
       auto const     volumeName = std::string(volume->GetName());
       if (not ddsurf->detElement().isValid())
         continue;
-      std::string path = ddsurf->detElement().path();
 
-      //FIXME: replace this with the ACTS type to be used as an extension
+      dd4hep::BitFieldCoder* cellIDcoder = nullptr;
+      if (ddsurf->type().isSensitive() and not ddsurf->type().isHelper()) {
+        // surface is sensitive, assuming it has a readout
+        auto detectorName = ddsurf->detElement().type();
+        try {
+          auto theDetector = dd4hep::SensitiveDetector(description.sensitiveDetector(detectorName));
+          cellIDcoder      = theDetector.readout().idSpec().decoder();
+          printout(PrintLevel::DEBUG, LOG_SOURCE, "The encoding string for detector %s is %s", detectorName.c_str(),
+                   cellIDcoder->fieldDescription().c_str());
+        } catch (...) {
+          printout(PrintLevel::INFO, LOG_SOURCE, "Not readout for detector %s", detectorName.c_str());
+          continue;
+        }
+      } else {
+        continue;
+      }
+      counter++;
+
+      std::string path        = ddsurf->detElement().path();
+      auto        layerNumber = cellIDcoder->get(ddsurf->id(), "layer");
+      printout(PrintLevel::INFO, LOG_SOURCE, "Found Surface at %s in layer %d at radius %3.2f mm", path.c_str(),
+               layerNumber, ddsurf->origin().rho());
+
+      //fixme: replace this with the ACTS type to be used as an extension
       dd4hep::rec::DoubleParameters* para = nullptr;
       try {  // use existing map, or create a new one
         para                                    = ddsurf->detElement().extension<dd4hep::rec::DoubleParameters>();
@@ -42,14 +70,12 @@ namespace {
         para->doubleParameters["SortingPolicy"] = 321;
         ddsurf->detElement().addExtension<dd4hep::rec::DoubleParameters>(para);
       }
-      printout(PrintLevel::INFO, LOG_SOURCE,
-               "Added extension to %s"
-               " path %s, type %s, value %3.5f",
-               volumeName.c_str(), ddsurf->detElement().path().c_str(), ddsurf->detElement().type().c_str(),
-               para->doubleParameters["SortingPolicy"]);
+      if (counter > 1000) {
+        break;
+      }
     }  // for all surfaces
 
-    return 0;  // this return value isn't really returned from the plugin service
+    return 42;
   }
 
 }  // namespace
