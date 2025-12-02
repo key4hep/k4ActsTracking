0832ea0a6567eaa56a5f61c77ea6f3a604be8daa
Thomas Madlener <thomas.madlener@desy.de>
2025-12-02 15:19:34 +0100

    First version of service for building Gen3 geometries

---
+++ k4ActsTracking/src/components/ActsGeoGen3Svc.h
@@ -0,0 +1,39 @@
+#ifndef K4ACTSTRACKING_ACTSGEOGEN3SVC_H
+#define K4ACTSTRACKING_ACTSGEOGEN3SVC_H
+
+#include "k4ActsTracking/IActsGeoSvc.h"
+
+#include <k4Interface/IGeoSvc.h>
+
+#include "GaudiKernel/Service.h"
+
+#include <memory>
+#include <string>
+
+namespace Acts {
+  class TrackingGeometry;
+}
+
+namespace dd4hep {
+  class Detector;
+}
+
+class ActsGeoGen3Svc : public extends<Service, IActsGeoSvc> {
+public:
+  const Acts::TrackingGeometry& trackingGeometry() const override;
+
+  ActsGeoGen3Svc(const std::string& name, ISvcLocator* svcLoc);
+
+  ~ActsGeoGen3Svc() = default;
+
+  StatusCode initialize() override;
+
+private:
+  dd4hep::Detector*                             m_dd4hepGeo{nullptr};
+  SmartIF<IGeoSvc>                              m_geoSvc;
+  std::unique_ptr<const Acts::TrackingGeometry> m_trackingGeo{nullptr};
+};
+
+inline const Acts::TrackingGeometry& ActsGeoGen3Svc::trackingGeometry() const { return *m_trackingGeo; }
+
+#endif  // K4ACTSTRACKING_ACTSGEOGEN3SVC_H
