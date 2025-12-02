9a9150d1e719d1b035221ff07f8077e922919eef
Thomas Madlener <thomas.madlener@desy.de>
2025-12-02 15:19:34 +0100

    [wip] First version of service for building Gen3 geometries

---
+++ k4ActsTracking/src/components/ActsGeoGen3Svc.cpp
@@ -0,0 +1,57 @@
+#include "ActsGeoGen3Svc.h"
+
+#include <k4FWCore/GaudiChecks.h>
+
+#include <Acts/Definitions/Units.hpp>
+#include <Acts/Geometry/Blueprint.hpp>
+#include <Acts/Geometry/BlueprintOptions.hpp>
+#include <Acts/Geometry/GeometryContext.hpp>
+#include <Acts/Geometry/GeometryIdentifier.hpp>
+#include <Acts/Geometry/TrackingGeometry.hpp>
+#include <Acts/Utilities/AxisDefinitions.hpp>
+#include <Acts/Visualization/ObjVisualization3D.hpp>
+#include <ActsPlugins/DD4hep/BlueprintBuilder.hpp>
+
+#include <DD4hep/DetElement.h>
+#include <DD4hep/Detector.h>
+
+#include <GaudiKernel/StatusCode.h>
+
+DECLARE_COMPONENT(ActsGeoGen3Svc)
+
+ActsGeoGen3Svc::ActsGeoGen3Svc(const std::string& name, ISvcLocator* svcLoc) : base_class(name, svcLoc) {}
+
+StatusCode ActsGeoGen3Svc::initialize() {
+  m_geoSvc = Gaudi::svcLocator()->service<IGeoSvc>("GeoSvc");
+  K4_GAUDI_CHECK(m_geoSvc);
+
+  ActsPlugins::DD4hep::BlueprintBuilder builder{{
+      .dd4hepDetector = m_geoSvc->getDetector(),
+      .lengthScale    = Acts::UnitConstants::cm,
+  }};
+
+  using Acts::Experimental::Blueprint;
+  using Acts::Experimental::BlueprintOptions;
+  using namespace Acts::UnitLiterals;
+  using enum Acts::AxisDirection;
+
+  Blueprint::Config cfg;
+  // Padding around subvolumes of the world volume
+  cfg.envelope[AxisZ] = {20_mm, 20_mm};
+  cfg.envelope[AxisR] = {0_mm, 20_mm};
+  Blueprint root{cfg};
+
+  auto innerTrackerBarrelElem = builder.findDetElementByName("InnerTrackerBarrel").value();
+  auto innerTrackerBarrel     = builder.addLayers(innerTrackerBarrelElem, "XYZ", AxisR, std::regex{"layer\\d_\\d"});
+
+  BlueprintOptions      options;
+  Acts::GeometryContext gctxt{};
+
+  m_trackingGeo = root.construct(options, gctxt);
+
+  Acts::ObjVisualization3D vis{};
+  m_trackingGeo->visualize(vis, gctxt);
+  vis.write("dumped_acts_geo.obj");
+
+  return StatusCode::SUCCESS;
+}
