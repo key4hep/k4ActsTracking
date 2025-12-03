ab05f389860e6aa1af094628d72890e7d3093c1d
Thomas Madlener <thomas.madlener@desy.de>
2025-12-03 11:23:17 +0100

    Make selection configurable and add example

--- k4ActsTracking/src/components/ActsGeoGen3Svc.cpp
+++ k4ActsTracking/src/components/ActsGeoGen3Svc.cpp
@@ -1,10 +1,14 @@
 #include "ActsGeoGen3Svc.h"
 
+#include "k4ActsTracking/ActsGaudiLogger.h"
+
 #include <k4FWCore/GaudiChecks.h>
 
 #include <Acts/Definitions/Units.hpp>
 #include <Acts/Geometry/Blueprint.hpp>
+#include <Acts/Geometry/BlueprintNode.hpp>
 #include <Acts/Geometry/BlueprintOptions.hpp>
+#include <Acts/Geometry/ContainerBlueprintNode.hpp>
 #include <Acts/Geometry/GeometryContext.hpp>
 #include <Acts/Geometry/GeometryIdentifier.hpp>
 #include <Acts/Geometry/TrackingGeometry.hpp>
@@ -25,10 +29,13 @@ StatusCode ActsGeoGen3Svc::initialize() {
   m_geoSvc = Gaudi::svcLocator()->service<IGeoSvc>("GeoSvc");
   K4_GAUDI_CHECK(m_geoSvc);
 
+  auto gaudiLogger = makeActsGaudiLogger(this);
+
   ActsPlugins::DD4hep::BlueprintBuilder builder{{
-      .dd4hepDetector = m_geoSvc->getDetector(),
-      .lengthScale    = Acts::UnitConstants::cm,
-  }};
+                                                    .dd4hepDetector = m_geoSvc->getDetector(),
+                                                    .lengthScale    = Acts::UnitConstants::cm,
+                                                },
+                                                gaudiLogger->cloneWithSuffix("BlpBld")};
 
   using Acts::Experimental::Blueprint;
   using Acts::Experimental::BlueprintOptions;
@@ -41,14 +48,23 @@ StatusCode ActsGeoGen3Svc::initialize() {
   cfg.envelope[AxisR] = {0_mm, 20_mm};
   Blueprint root{cfg};
 
-  auto innerTrackerBarrelElem = builder.findDetElementByName("InnerTrackerBarrel").value();
-  auto innerTrackerBarrel     = builder.addLayers(innerTrackerBarrelElem, "XYZ", AxisR, std::regex{"layer\\d_\\d"});
+  debug() << "Finding " << m_detElementName.value() << " detector element" << endmsg;
+  auto innerTrackerBarrelElem = builder.findDetElementByName(m_detElementName.value()).value();
+  debug() << "Adding layers from " << m_detElementName.value() << " matching " << m_layerPattern.value() << endmsg;
+  auto innerTrackerBarrel = builder.addLayers(innerTrackerBarrelElem, "XYZ", AxisR, std::regex{m_layerPattern.value()});
+
+  debug() << "Found " << innerTrackerBarrel->children().size() << " children in innerTrackerBarrel" << endmsg;
+
+  debug() << "Adding inner tracker barrel to the root blueprint" << endmsg;
+  root.addChild(innerTrackerBarrel);
 
   BlueprintOptions      options;
   Acts::GeometryContext gctxt{};
 
+  debug() << "Constructing tracking geometry" << endmsg;
   m_trackingGeo = root.construct(options, gctxt);
 
+  debug() << "Creating visualiztion" << endmsg;
   Acts::ObjVisualization3D vis{};
   m_trackingGeo->visualize(vis, gctxt);
   vis.write("dumped_acts_geo.obj");
