5265bbe5298a281b2de183969bc2ce7469e87a53
Thomas Madlener <thomas.madlener@desy.de>
2026-03-04 10:03:54 +0100

    [wip] Move blueprint population into library-like construct

---
+++ k4ActsTracking/src/components/DD4hepBlueprintConstruction.h
@@ -0,0 +1,38 @@
+#ifndef K4ACTSTRACKING_DD4HEPBLUEPRINTCONSTRUCTION_H
+#define K4ACTSTRACKING_DD4HEPBLUEPRINTCONSTRUCTION_H
+
+#include <Acts/Definitions/Units.hpp>
+
+#include <string>
+
+namespace Acts::Experimental {
+  class ContainerBlueprintNode;
+  class Blueprint;
+}  // namespace Acts::Experimental
+
+namespace ActsPlugins::DD4hep {
+  class BlueprintBuilder;
+}
+
+namespace Blueprints {
+  using namespace Acts::UnitLiterals;
+  void addCylindricalBeampipe(Acts::Experimental::ContainerBlueprintNode& root, double rMax = 10_mm,
+                              double halfZ = 1000_mm);
+}  // namespace Blueprints
+
+namespace MuColl {
+  namespace MAIA_v0 {
+    void populateBlueprint(const std::string& detName, Acts::Experimental::Blueprint& root,
+                           ActsPlugins::DD4hep::BlueprintBuilder& builder);
+  }
+}  // namespace MuColl
+
+namespace FCCee {
+  namespace ILD_FCCee {
+    void populateBlueprint(const std::string& detName, Acts::Experimental::Blueprint& root,
+                           ActsPlugins::DD4hep::BlueprintBuilder& builder);
+  }
+  // namespace FCCee
+}  // namespace FCCee
+
+#endif  // K4ACTSTRACKING_DD4HEPBLUEPRINTCONSTRUCTION_H
