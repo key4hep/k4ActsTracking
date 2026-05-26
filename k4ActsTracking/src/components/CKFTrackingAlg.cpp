735966852dfbfc3999c2a899da8e10ff1266f58b
Federico Meloni <madb4ron@gmail.com>
2026-05-26 15:35:39 +0200

    made units explicit in property description

--- k4ActsTracking/src/components/CKFTrackingAlg.cpp
+++ k4ActsTracking/src/components/CKFTrackingAlg.cpp
@@ -167,9 +167,8 @@ private:
                                                        "Number of sigmas allowed in scattering angle."};
   Gaudi::Property<float> m_seedFinding_radLengthPerSeed{this, "SeedFinding_RadLengthPerSeed", 0.1,
                                                         "Average radiation length per seed."};
-  Gaudi::Property<float> m_seedFinding_minPt{this, "SeedFinding_MinPt", 500.0, "Minimum pT of tracks to seed."};
-  Gaudi::Property<float> m_seedFinding_impactMax{this, "SeedFinding_ImpactMax", 3.0 * Acts::UnitConstants::mm,
-                                                 "Maximum d0 of tracks to seed."};
+  Gaudi::Property<float> m_seedFinding_minPt{this, "SeedFinding_MinPt", 500.0, "Minimum pT of tracks to seed [MeV]."};
+  Gaudi::Property<float> m_seedFinding_impactMax{this, "SeedFinding_ImpactMax", 3.0, "Maximum d0 of tracks to seed [mm]."};
 
   std::vector<std::string>                  m_default_empty_vec;
   Gaudi::Property<std::vector<std::string>> m_seedFinding_zBinEdges{this, "SeedFinding_zBinEdges", m_default_empty_vec,
