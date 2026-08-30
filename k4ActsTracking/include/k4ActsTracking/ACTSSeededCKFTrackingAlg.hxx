4f4ab659ed46ca23a2b1b4cd18fd04a6bf970ac7
Federico Meloni <madb4ron@gmail.com>
2026-08-30 15:08:36 +0200

    consume upstream acts converters

--- k4ActsTracking/include/k4ActsTracking/ACTSSeededCKFTrackingAlg.hxx
+++ k4ActsTracking/include/k4ActsTracking/ACTSSeededCKFTrackingAlg.hxx
@@ -109,8 +109,7 @@ public:
   StatusCode tracking(const std::vector<Acts::BoundTrackParameters>& paramseeds, const CKF& trackFinder,
                       const TrackFinderOptions& ckfOptions, const Propagator& extrapPropagator,
                       const Acts::PerigeeSurface& perigeeSurface, Propagator::Options<>& extrapOptions,
-                      const ACTSTracking::HitContainer& hits, Acts::MagneticFieldProvider::Cache& magCache,
-                      edm4hep::TrackCollection& trackCollection) const;
+                      const ACTSTracking::HitContainer& hits, edm4hep::TrackCollection& trackCollection) const;
 
 protected:
   /**
