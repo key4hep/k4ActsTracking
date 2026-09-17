40b0967bc9596c25449d9179d1ec19eb5bae5ed2
Juan Miguel Carceller <22276694+jmcarcell@users.noreply.github.com>
2026-09-17 13:53:42 +0200

    Fix compatibility with updated ACTS APIs (#98)

--- k4ActsTracking/src/components/ACTSSeededCKFTrackingAlg.cxx
+++ k4ActsTracking/src/components/ACTSSeededCKFTrackingAlg.cxx
@@ -270,8 +270,8 @@ ACTSSeededCKFTrackingAlg::operator()(const edm4hep::TrackerHitPlaneCollection& t
       }
     }
   }
-  gridCfg.bottomBinFinder.emplace(m_phiBottomBinLen.value(), m_zBottomBinLen.value(), 0);
-  gridCfg.topBinFinder.emplace(m_phiTopBinLen.value(), m_zTopBinLen.value(), 0);
+  gridCfg.bottomBinFinder = Acts::GridBinFinder<3>{m_phiBottomBinLen.value(), m_zBottomBinLen.value(), 0};
+  gridCfg.topBinFinder = Acts::GridBinFinder<3>{m_phiTopBinLen.value(), m_zTopBinLen.value(), 0};
 
   Acts::CylindricalSpacePointGrid grid(gridCfg, Acts::getDefaultLogger("SeededCKFSeedingGrid", Acts::Logging::WARNING));
 
